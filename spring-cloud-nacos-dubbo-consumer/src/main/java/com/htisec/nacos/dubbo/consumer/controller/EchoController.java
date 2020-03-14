47f3e896fdb54ae92cafde61f94cbf9368c135ca
Jeremy Zhou <jeremygilbert@126.com>
2020-03-14 21:23:13 +0800

    change nacos address

--- spring-cloud-nacos-dubbo-consumer/src/main/java/com/htisec/nacos/dubbo/consumer/controller/EchoController.java
+++ spring-cloud-nacos-dubbo-consumer/src/main/java/com/htisec/nacos/dubbo/consumer/controller/EchoController.java
@@ -4,13 +4,12 @@ import com.htisec.nacos.dubbo.provider.api.EchoService;
 
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
+//@RefreshScope
 public class EchoController {
 
     @Reference(version = "1.0.0")
